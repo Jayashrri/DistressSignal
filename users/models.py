337719109be6968e3f9557b05a33e51b0b58d424
jayashrri <balaji.jayashrri@gmail.com>
2019-12-22 01:32:56 +0530

    Add signal for updation of Profile model with User

--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 import uuid
 
@@ -16,6 +18,15 @@ class Profile(models.Model):
     name = models.CharField(max_length=100)
     phone = models.CharField(max_length=15)
     distress = models.BooleanField(default=False)
+
+@receiver(post_save, sender=User)
+def create_user(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user(sender, instance, **kwargs):
+    instance.Profile.save()
 
 class Tracker(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
