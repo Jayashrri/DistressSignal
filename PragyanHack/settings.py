b8eff92a377bdc197343bd991cb9969caced7b75
jayashrri <balaji.jayashrri@gmail.com>
2019-12-22 00:01:40 +0530

    Change DB to SQLite

--- PragyanHack/settings.py
+++ PragyanHack/settings.py
@@ -85,12 +85,8 @@ WSGI_APPLICATION = 'PragyanHack.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'phack',
-        'USER': os.environ['USER'],
-        'PASSWORD': os.environ['PASSWORD'],
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
