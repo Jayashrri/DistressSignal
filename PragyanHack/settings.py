467480c22c5c24ad6b7b3c370b6c5831eaeed19e
jayashrri <balaji.jayashrri@gmail.com>
2019-12-22 04:37:57 +0530

    Create custom user model from AbstractUser

--- PragyanHack/settings.py
+++ PragyanHack/settings.py
@@ -90,6 +90,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = "users.Profile"
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
--- users/models.py
+++ users/models.py
@@ -1,34 +1,25 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.conf import settings
 
 import uuid
 
 # Create your models here.
 
-class Location(models.Model):
-    latitude = models.CharField(max_length=20)
-    longitude = models.CharField(max_length=20)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    timestamp = models.DateTimeField(auto_now_add=True)
-
-class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+class Profile(AbstractUser):
     name = models.CharField(max_length=100)
     phone = models.CharField(max_length=15)
     distress = models.BooleanField(default=False)
 
-@receiver(post_save, sender=User)
-def create_user(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user(sender, instance, **kwargs):
-    instance.Profile.save()
+class Location(models.Model):
+    latitude = models.CharField(max_length=20)
+    longitude = models.CharField(max_length=20)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
 class Tracker(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     uid = models.CharField(default=uuid.uuid1(), max_length=100, blank=False, null=False)
 
